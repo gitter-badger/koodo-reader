027ee6f98c3425cb81a1686c6b3357fd49d53483
troyeguo <guo362429@163.com>
2020-03-01 13:32:39 +0800

    test

---
+++ build/precache-manifest.c62642b75b2b4aea411ca583283628bd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f2cfc1cb721e6ae2b41ca3ab71508f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "531ad665010ed263fc7e",
+    "url": "/static/css/main.5209c677.chunk.css"
+  },
+  {
+    "revision": "1f824207d22eb5b9c555",
+    "url": "/static/js/2.5b9a2fbf.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.5b9a2fbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "531ad665010ed263fc7e",
+    "url": "/static/js/main.efe0d978.chunk.js"
+  },
+  {
+    "revision": "71345d931e8b1304ff90",
+    "url": "/static/js/runtime-main.e14e7949.js"
+  },
+  {
+    "revision": "0e338fd65cf933356635138d55514a00",
+    "url": "/static/media/icomoon.0e338fd6.ttf"
+  },
+  {
+    "revision": "783fc04fcbf41f1beafb801beb3ad548",
+    "url": "/static/media/icomoon.783fc04f.eot"
+  },
+  {
+    "revision": "c251f1cabffcc5a285058620f13c601c",
+    "url": "/static/media/icomoon.c251f1ca.woff"
+  },
+  {
+    "revision": "d66feae9cdeedc309e820c1b19f3580e",
+    "url": "/static/media/icomoon.d66feae9.svg"
+  }
+]);
