465a2fe17e7f013ce62399324252d506d4bb430b
troyeguo <guo362429@163.com>
2020-03-04 13:51:36 +0800

    test

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store";
-import "react-app-polyfill/ie9";
-import "react-app-polyfill/ie11";
-import "react-app-polyfill/stable";
 // console.log(window.devToolsExtension, "hello1");
 ReactDOM.render(
   <Provider store={store}>
